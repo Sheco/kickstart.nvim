28ab2d264e4cce5c5bab67cdd6bbbc9c7349d810
Sergio Durán <sergio@tiene.info>
2025-08-23 08:58:57 -0600

    Improve neo-tree keymap

--- init.lua
+++ init.lua
@@ -181,7 +181,28 @@ vim.opt.spelllang = 'en_us'
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
-vim.keymap.set('n', '-', ':Neotree filesystem reveal position=current<cr>')
+vim.keymap.set('n', '-', function()
+  local reveal_file = vim.fn.expand '%:p'
+  if reveal_file == '' then
+    reveal_file = vim.fn.getcwd()
+  else
+    local f = io.open(reveal_file, 'r')
+    if f then
+      f.close(f)
+    else
+      reveal_file = vim.fn.getcwd()
+    end
+  end
+  require('neo-tree.command').execute {
+    action = 'focus', -- OPTIONAL, this is the default value
+    source = 'filesystem', -- OPTIONAL, this is the default value
+    position = 'current', -- OPTIONAL, this is the default value
+    reveal_file = reveal_file, -- path to file or folder to reveal
+    reveal_force_cwd = true, -- change cwd without asking if needed
+  }
+end, { desc = 'Open neo-tree at current file or working directory' })
+
+--  ':Neotree filesystem reveal position=current<cr>')
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`

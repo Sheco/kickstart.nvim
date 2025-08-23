cbd63066f37e51520080b71bd8b97d1007b69df2
Sergio Durán <sergio@tiene.info>
2025-08-23 10:24:16 -0600

    Enable zeek LSP the neovim 0.11 way

---
+++ lsp/zeek.lua
@@ -0,0 +1,6 @@
+return {
+  cmd = { 'zeek-language-server' },
+  filetypes = { 'zeek' },
+  root_markers = { '.git' },
+  settings = {},
+}
--- lua/custom/plugins/zeek.lua
+++ lua/custom/plugins/zeek.lua
@@ -1,10 +1,4 @@
-vim.lsp.config['zeek'] = {
-  cmd = { 'zeek-language-server' },
-  filetypes = { 'zeek' },
-  root_markers = { '.git' },
-  settings = {},
-}
-vim.lsp.enable 'zeek'
+vim.lsp.enable { 'zeek' }
 
 return {
   { 'zeek/vim-zeek', enabled = true },

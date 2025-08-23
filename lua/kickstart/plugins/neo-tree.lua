551ac32af7f5c540ab6e93ec6a15bc007843d336
Sergio Durán <sergio@tiene.info>
2025-08-23 10:38:44 -0600

    improve neo-tree keymaps

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,36 +12,14 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    {
-      '-',
-      function()
-        local reveal_file = vim.fn.expand '%:p'
-        if reveal_file == '' then
-          reveal_file = vim.fn.getcwd()
-        else
-          local f = io.open(reveal_file, 'r')
-          if f then
-            f.close(f)
-          else
-            reveal_file = vim.fn.getcwd()
-          end
-        end
-        require('neo-tree.command').execute {
-          action = 'focus', -- OPTIONAL, this is the default value
-          source = 'filesystem', -- OPTIONAL, this is the default value
-          position = 'current', -- OPTIONAL, this is the default value
-          reveal_file = reveal_file, -- path to file or folder to reveal
-          reveal_force_cwd = true, -- change cwd without asking if needed
-        }
-      end,
-      { desc = 'Open neo-tree at current file or working directory' },
-    },
+    { '-', ':Neotree filesystem reveal position=current<CR>', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['-'] = 'close_window',
         },
       },
     },

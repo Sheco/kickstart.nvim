2dcdc7f0f6946e563e78c124221e0826294b2539
Sergio Durán <sergio@tiene.info>
2025-09-03 10:57:06 -0600

    sort buffers by last used

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -7,12 +7,15 @@ return {
     options = {
       always_show_bufferline = true,
       sort_by = function(buffer_a, buffer_b)
-        local stat_a = vim.loop.fs_stat(buffer_a.path)
-        local stat_b = vim.loop.fs_stat(buffer_b.path)
-        if not stat_a or not stat_b then
-          return (stat_a and true) or false
-        end
-        return stat_a.mtime.sec > stat_b.mtime.sec
+        local info_a = vim.fn.getbufinfo(buffer_a.id)[1]
+        local info_b = vim.fn.getbufinfo(buffer_b.id)[1]
+        return info_a.lastused > info_b.lastused
+        -- local stat_a = vim.loop.fs_stat(buffer_a.path)
+        -- local stat_b = vim.loop.fs_stat(buffer_b.path)
+        -- if not stat_a or not stat_b then
+        --   return (stat_a and true) or false
+        -- end
+        -- return stat_a.mtime.sec > stat_b.mtime.sec
       end,
     },
   },

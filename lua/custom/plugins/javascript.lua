8df6d78b17a359c67ccc4b863b9218b83ad47d06
Sergio Durán <sergio@tiene.info>
2025-10-02 10:09:54 -0600

    fix: don't use require('lspconfig')

--- lua/custom/plugins/javascript.lua
+++ lua/custom/plugins/javascript.lua
@@ -1,5 +1,5 @@
 -- Add this inside your LSP setup code
 
 -- for some reason I can't make vtsls active without the following line
-require('lspconfig').vtsls.setup {}
+--require('lspconfig').vtsls.setup {}
 return {}

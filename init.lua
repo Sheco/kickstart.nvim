90d16ccf7e36ffb426b109fcc1f18799cc03419e
Sergio Durán <sergio@tiene.info>
2026-02-01 22:09:41 -0600

    feat: auto install typescript and bash language servers

--- init.lua
+++ init.lua
@@ -623,7 +623,9 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua-language-server', -- Lua Language server
+        'lua_ls', -- Lua Language server
+        'ts_ls',
+        'bashls',
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
